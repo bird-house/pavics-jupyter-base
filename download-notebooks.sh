3c659dfae8bbb5cdfe1b888a1b7d17b4271bec7b
Charles-William Cummings <cwcummings1@gmail.com>
2021-03-31 08:14:17 -0400

    rename variable in script

--- download-notebooks.sh
+++ download-notebooks.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Notebook directory used by JupyterHub in birdhouse-deploy
-notebook_dir="/notebook_dir/tutorial-notebooks"
+NOTEBOOK_DIR="/notebook_dir/tutorial-notebooks"
 
 # Download notebooks required for the base image and add them to the notebook directory
 wget -O - https://github.com/Ouranosinc/pavics-sdi/archive/master.tar.gz | \
-    tar -xz --wildcards -C $notebook_dir --strip=4 "*/docs/source/notebooks/jupyter_extensions.ipynb"
+    tar -xz --wildcards -C $NOTEBOOK_DIR --strip=4 "*/docs/source/notebooks/jupyter_extensions.ipynb"
 
 # Remove write permission on the tutorial-notebooks
-chmod -R 555 $notebook_dir/*
+chmod -R 555 $NOTEBOOK_DIR/*
